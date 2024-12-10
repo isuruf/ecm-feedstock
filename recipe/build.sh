df391015974de7427a33593b9eb25ec6997bcafe
Isuru Fernando <isuruf@gmail.com>
2024-12-10 08:30:36 +0530

    add windows support

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,24 +1,29 @@
 #!/usr/bin/env bash
-# Get an updated config.sub and config.guess
-cp $BUILD_PREFIX/share/libtool/build-aux/config.* .
 
-export LD_LIBRARY_PATH=$PREFIX/lib:$LD_LIBRARY_PATH
-
-if [ "$(uname)" == "Linux" ]
-then
-    autoconf
+if [[ "$target_platform" != "win-"* ]]; then
+  # Get an updated config.sub and config.guess
+  cp $BUILD_PREFIX/share/libtool/build-aux/config.* .
 fi
 
-chmod +x configure
-./configure --prefix=$PREFIX --with-gmp-include=$PREFIX/include --with-gmp-lib=$PREFIX/lib --enable-shared
+autoreconf -vfi
 
-make
+chmod +x configure
+./configure \
+  --prefix=$PREFIX \
+  --with-gmp-include=$PREFIX/include \
+  --with-gmp-lib=$PREFIX/lib \
+  --enable-shared \
+  --disable-static
+
+[[ "$target_platform" == "win-"* ]] && patch_libtool
+
+make -j${CPU_COUNT}
 
 chmod +x test.pp1
 chmod +x test.pm1
 chmod +x test.ecm
-if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
-make check
+if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
+  make check
 fi
 
 make install
